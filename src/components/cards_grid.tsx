9d4f70600eb2c6fff33c6b461a3ec59efc40ba5a
Marlon <marlon.colca.io@gmail.com>
2025-01-16 06:10:26 -0500

    win logic, working on sthetics

--- src/components/cards_grid.tsx
+++ src/components/cards_grid.tsx
@@ -2,12 +2,13 @@ import { useStore } from '@nanostores/react'
 import { CardImage } from '../models/card_image'
 import Card from './card'
 import { $cards } from '../stores/card_store'
-import { $isSelectedAlready, $isFlippedAlready } from '../stores/game_core_store'
+import { $isSelectedAlready, $isFlippedAlready, $gameStage } from '../stores/game_core_store'
 
 export default function CardsGrid() {
   const cards = useStore($cards)
   const isSelectedAlready = useStore($isSelectedAlready)
   const isFlippedAlready = useStore($isFlippedAlready)
+  const gameStage = useStore($gameStage)
   
   const renderCards = () => {
     return cards.map((cardImage: CardImage) => (
@@ -25,8 +26,7 @@ export default function CardsGrid() {
   // ! TODO: Problem with this columns
   return (
     <>
-    <div>{ `isSelectedAlready=${JSON.stringify(isSelectedAlready)}` }</div>
-    <div>{ `isFlippedAlready=${JSON.stringify(isFlippedAlready)}` }</div>
+    <div>{ `gameStage=${JSON.stringify(gameStage)}` }</div>
       <div className="grid grid-cols-8 gap-4 justify-items-center">
         
         { renderCards() }
--- src/stores/game_core_store.ts
+++ src/stores/game_core_store.ts
@@ -4,6 +4,7 @@ import { $selectedDifficulty } from '../stores/game_settings_store'
 
 export const $selectedCards = atom<CardImage[]>([])
 export const $flippedCards = atom<CardImage[]>([])
+export const $gameStage = atom<"PLAYING" | "WON" | "LOSE">("PLAYING")
 
 export const $isSelectedAlready = computed(
   $selectedCards,
@@ -18,8 +19,7 @@ export const $isFlippedAlready = computed(
 
 export function playCard (card: CardImage) {
   if ($selectedCards.value.length === 0) {
-    $selectedCards.value.push(card)
-    $selectedCards.set([...$selectedCards.value])
+    $selectedCards.set([card])
 
     console.log('ITS ALONE, FLIP IT')
     return
@@ -32,24 +32,29 @@ export function playCard (card: CardImage) {
   $selectedCards.value.push(card)
   $selectedCards.set([...$selectedCards.value])
 
-  // ? Check if card is already selected
-  if ($selectedCards.value[0].image_id === card.image_id) {
-    
-    $selectedCards.set([...$selectedCards.value])
-
-    console.log('ITS A MATCH! FLIP THEM')
-    // ? check if all cards requirement are selected
-    if ($selectedDifficulty.value.itemsToCollect >= $selectedCards.value.length) {
-      $flippedCards.set([...$flippedCards.value, ...$selectedCards.value])
-      $selectedCards.set([])
-
-      console.log('FLIPPED NOW!')
-    }
-  } else {
+  if ($selectedCards.value[0].image_id !== card.image_id) {
     console.log('ITS NOT A MATCH! FLIP THEM BACK')
     window.setTimeout(() => {
       $selectedCards.set([])
     }, 750)
+    return
+  }
+
+  console.log('ITS A MATCH! CHECK IF YOU MEET ALL REQUIREMENTS!')
+  const { cardsCount, itemsToCollect } = $selectedDifficulty.value
+
+  // ? check if all cards requirement are selected
+  if (itemsToCollect >= $selectedCards.value.length) {
+    $flippedCards.set([...$flippedCards.value, ...$selectedCards.value])
+    $selectedCards.set([])
+
+    console.log('FLIPPED NOW!')
+  }
+
+  // ? check if all cards are flipped
+  if ($flippedCards.value.length === cardsCount * itemsToCollect) {
+    $gameStage.set("WON")
+    console.log("I CAN'T! BELIEVE IT, YOU WON!")
   }
 
 }
